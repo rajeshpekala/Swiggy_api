d5ffe128f0272b24390246f17b8750e3fca04cdd
rajeshpekala <pekalarajesh001@gmail.com>
2024-07-10 02:31:57 +0530

    added data

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const PORT = process.env.PORT || 3000;
 
 // Sample JSON data (replace this with your actual JSON data)
 const data = require("./data.json");
+const data1 = require("./data1.json");
 
 // Use CORS middleware
 app.use(cors());
@@ -12,6 +13,10 @@ app.use(cors());
 app.get("/api/data", (req, res) => {
   res.json(data);
 });
+app.get("api/resId",(req,res)=>{
+  res.json(data1)
+})
+
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
