559f1917907f9f9f2a2cf973f649670af59e3032
rajeshpekala <pekalarajesh001@gmail.com>
2024-07-09 23:08:52 +0530

    added things

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-
+// Sample JSON data (replace this with your actual JSON data)
 const data = require("./data.json");
+
+// Use CORS middleware
+app.use(cors());
 
 app.get("/api/data", (req, res) => {
   res.json(data);
